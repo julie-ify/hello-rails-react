49adf4328d60de3321a13705c2558eb6dbcedcf6
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 18:04:09 +0100

    Fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
     get 'messages', to: 'messages#index'
   end
 
-  # get '*page', to: 'static#index', constraints: ->(req) do
-  #   !req.xhr? && req.format.html?
-  # end
-
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
