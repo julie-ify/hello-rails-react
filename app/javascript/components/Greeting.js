b8da3f50e18f7f8dc4b28487ce71bc1d5495a72c
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 18:03:56 +0100

    Display random greetings from my API end point

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,16 +1,54 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const GET_MESSAGES_REQUEST = 'GET_MESSAGES_REQUEST';
+const GET_MESSAGES_SUCCESS = 'GET_MESSAGES_SUCCESS';
+
+function getMessages() {
+  console.log('getMessages() Action!!');
+  return (dispatch) => {
+    dispatch({ type: GET_MESSAGES_REQUEST });
+    return fetch(`v1/messages.json`)
+      .then((response) => response.json())
+      .then((json) => dispatch(getMessagesSuccess(json)))
+      .catch((error) => console.log(error));
+  };
+}
+
+export function getMessagesSuccess(json) {
+  return {
+    type: GET_MESSAGES_SUCCESS,
+    json,
+  };
+}
+
 class Greeting extends React.Component {
-  render () {
+  render() {
+    const { greetings } = this.props;
+    const messagesList = greetings.map((greet, index) => {
+      return <li key={index}> {greet.greeting} </li>;
+    });
     return (
-      <React.Fragment>
+      <div className='container'>
         Greeting: {this.props.greeting}
-      </React.Fragment>
+        <br />
+        <button onClick={() => this.props.getMessages()}>Get greeting</button>
+        <br />
+        <ul>{messagesList}</ul>
+      </div>
     );
   }
 }
 
+const structuredSelctor = createStructuredSelector({
+  greetings: (state) => state.greetings,
+});
+
+const mapDispatchToProps = { getMessages };
+
 Greeting.propTypes = {
-  greeting: PropTypes.string
+  greeting: PropTypes.string,
 };
-export default Greeting
+export default connect(structuredSelctor, mapDispatchToProps)(Greeting);
