bb6facf2e535de0dd5986f182b794cf0064502f1
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 15:44:13 +0100

    Configure route to render static views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { formart: 'json' } do
     get 'messages', to: 'messages#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
