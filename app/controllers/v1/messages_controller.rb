c3c1963b6992bbe679323995b285c78b5ed893ab
Juliana Ifionu <julieifionu@gmail.com>
2022-01-10 18:02:45 +0100

    Add redux store to App component

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class V1::MessagesController < ApplicationController
   def index
     @message = Message.all.order('random()').limit(1)
-    render json: { :greeting => @message }.to_json
+    render json: { :greetings => @message }.to_json
   end 
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,24 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Greeting from "./Greeting";
+import { Provider } from 'react-redux';
+import configureStore from '../redux/configureStore';
+import Greeting from './Greeting';
+
+const store = configureStore();
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Greeting greeting="Friend" />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Greeting greeting="Friend" />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
 
-export default App
+export default App;
